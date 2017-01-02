696aa83c29673b6aa00f9972c45d08944dca2b99
vladandr <vlad_a_1996@tut.by>
2017-01-02 18:46:45 +0300

    Added some display attributes

--- EasyCredit/Models/CreditPlan.cs
+++ EasyCredit/Models/CreditPlan.cs
@@ -1,16 +1,27 @@
-﻿using EasyCredit.Constants;
+﻿using System.ComponentModel.DataAnnotations;
+using EasyCredit.Constants;
 
 namespace EasyCredit.Models
 {//ready
     public class CreditPlan : EasyCreditEntity
     {
         public string Description { get; set; }
+
+        [Display(Name = "Name of plan")]
         public string NameOfPlan { get; set; }
+
+        [Display(Name = "Minimum value to credit")]
         public int MinValueToCredit { get; set; }
+
+        [Display(Name = "Maximum value to credit")]
         public int MaxValueToCredit { get; set; }
+
         public int LimitDays { get; set; }
+
         public int Surcharge { get; set; }
+
         public int Percentage { get; set; }
+
         public CreditPlanStatusDictionary Status { get; set; }
     }
 }
